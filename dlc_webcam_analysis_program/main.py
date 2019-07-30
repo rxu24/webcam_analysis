26895183b95c443da71861039884cf1b54c4aedc
rxu24 <rxu24@uic.edu>
2019-07-30 12:14:31 -0500

    moved csv reading and plotting scatter functions using matplotlib to its own python file for greater clarity in main file, be careful because changing directory inside a function will impact the entire program, works in my favor currently but may need to be adjusted depending on what I want my working directory to be

--- dlc_webcam_analysis_program/main.py
+++ dlc_webcam_analysis_program/main.py
@@ -1,10 +1,9 @@
 import deeplabcut
 import os
-import pandas as pd
 import numpy as np
 from datetime import date
 from video_functions import capture_video, play_video
-import matplotlib.pyplot as plt
+from plot_scatter import plot_scatter
 
 # Main function with code to run
 if __name__ == "__main__":
@@ -20,7 +19,9 @@ if __name__ == "__main__":
 
     # Config Path
     config_path = 'D:/DeepLabCut/my_projects/Project2/config.yaml'
-    video_path = ('C:/Users/ruidi/OneDrive/Documents/GitProjects/webcam_analysis/dlc_webcam_analysis_program/' + day_of_week + '/' + str(my_date) + '/')
+    video_path = (
+            'C:/Users/ruidi/OneDrive/Documents/GitProjects/webcam_analysis/dlc_webcam_analysis_program/' + day_of_week + '/' + str(
+        my_date) + '/')
     # input('Please enter the directory of the video folder:')
     # 'C:Users/ruidi/OneDrive/Documents/GitProjects/webcam_analysis/dlc_webcam_analysis_program/' + day_of_week + '/' + str(date)
 
@@ -31,46 +32,10 @@ if __name__ == "__main__":
                                     trailpoints=5)
 
     # Use OpenCV to play the labeled video created by deeplabcut
-    #play_video(date, day_of_week)
+    # play_video(date, day_of_week)
 
-    # Csv file analysis
-    # Store the csv data into the dataframe starting from the third frame (header = 2)
-    df = pd.read_csv('outputDeepCut_resnet50_Project2Jul8shuffle1_125000.csv', header=2, index_col='coords')
-    print(df)
-    #print(type(df['bodyparts'][0])) # Read and print a dataframe using pandas library
+    # Plot a scatter plot from the csv data of the video taken
+    # At this point, current working directory is inside the file where the video and csv file are due to the change of directory in the capture_video function
+    plot_scatter()
 
-    shoulder = df.iloc[:,[0,1]]
-    print(shoulder)
-
-    elbow = df.iloc[:,[3,4]]
-    print(elbow)
-
-    wrist = df.iloc[:,[6,7]]
-    print(wrist)
-    
-    hand = df.iloc[:,[9,10]]
-    print(hand)
-
-    # Plot dataframes
-    #shoulder.plot(kind='scatter', x='x', y='y', color='darkblue')
-    ax = elbow.plot(kind='scatter', x='x.1', y='y.1', color='cyan', label='elbow')
-    wrist.plot(kind='scatter', x='x.2', y='y.2', color='yellow', ax=ax, label='wrist')
-    hand.plot(kind='scatter', x='x.3', y='y.3', color='red', ax=ax, label='hand')
-
-    # Set domain and range for plot
-    plt.ylim([0,480])
-    plt.xlim([0,600])
-
-    # Legend for graph
-    plt.legend(loc= 'upper left')
-
-    # Label title and axes
-    plt.suptitle('Location of Body Parts in 2D Space')
-    plt.xlabel('X position')
-    plt.ylabel('Y position')
-
-    #Display plot
-    plt.show()
-
-    #Other plotting methods
-    
+    # Other plotting methods
---
+++ dlc_webcam_analysis_program/plot_scatter.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+def plot_scatter():
+    # Csv file analysis
+    # Store the csv data into the dataframe starting from the third frame (header = 2)
+    print(os.getcwd())
+    df = pd.read_csv('outputDeepCut_resnet50_Project2Jul8shuffle1_125000.csv', header=2, index_col='coords')
+    print(df)
+    # print(type(df['bodyparts'][0])) # Read and print a dataframe using pandas library
+
+    shoulder = df.iloc[:, [0, 1]]
+    print(shoulder)
+
+    elbow = df.iloc[:, [3, 4]]
+    print(elbow)
+
+    wrist = df.iloc[:, [6, 7]]
+    print(wrist)
+
+    hand = df.iloc[:, [9, 10]]
+    print(hand)
+
+    # Plot dataframes
+    # shoulder.plot(kind='scatter', x='x', y='y', color='darkblue')
+    ax = elbow.plot(kind='scatter', x='x.1', y='y.1', color='cyan', label='elbow')
+    wrist.plot(kind='scatter', x='x.2', y='y.2', color='yellow', ax=ax, label='wrist')
+    hand.plot(kind='scatter', x='x.3', y='y.3', color='red', ax=ax, label='hand')
+
+    # Set domain and range for plot
+    plt.ylim([0, 480])
+    plt.xlim([0, 600])
+
+    # Legend for graph
+    plt.legend(loc='upper left')
+
+    # Label title and axes
+    plt.suptitle('Location of Body Parts in 2D Space')
+    plt.xlabel('X position')
+    plt.ylabel('Y position')
+
+    # Display plot
+    plt.show()
